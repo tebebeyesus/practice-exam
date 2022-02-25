43a505d4485a8db4d8a29f7cde821d8491cbbeeb
tebebeyesus garedew <tebebe@eboltsolutions.com>
2022-02-24 22:17:11 -0500

    made the necessary changes

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Obtain the img element, and assign it to the image variable
-let image 
+let image = document.querySelector('#vacation-spot');
+image.src ="https://upload.wikimedia.org/wikipedia/en/c/ca/Sunrise_over_Langano%2C_Ethiopia.jpg";
 
 // Obtain the 'flip-button' button, and assign it to the flipButton variable
-let flipButton 
-
+let flipButton = document.querySelector('#flip-button');
+console.log(flipButton);
 //DO NOT CHANGE THE FOLLOWING CODE
 flipButton.addEventListener("click", function() {
   if(image.style.transform == "rotate(180deg)") {
